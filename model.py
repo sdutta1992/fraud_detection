54984e181391c59dedd1f9740ad3c97ae612d311
sdutta1992 <soumyadeep.duta27@gmail.com>
2024-03-29 21:08:07 +0530

    added ml model codes

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print("Adding all ml related code")
+
+def model1():
+    print('Adding code for model1')
+
+def model2():
+    print('Adding code for model2')
+
+def model3():
+    print('Adding code for model3')
